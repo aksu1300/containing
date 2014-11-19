a89d7b9c1697f9852a373e5e45f8550dfaed76c3
Driving Ghost <Driving Ghost@DGPC1993.lan.nhl.nl>
2014-11-19 12:25:20 +0100

    #1 Pushing AGV and my main to my branch

--- src/containing/AGV.java
+++ src/containing/AGV.java
@@ -1,18 +1,43 @@
 package containing;
 
+import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Driving Ghost
  */
-public class AGV {
-    
-    AGV(){
+public class AGV extends Node {
+
+    Integer X;
+    Integer Y;
+    Material material;
+    Spatial model;
+    float speed;
+    String ID;
+
+    public AGV(String ID, Material material, AssetManager assetManager) {
+        this.ID = ID;
+        this.material = material;
         
+        model = assetManager.loadModel("Models/agv.j3o");
+        model.setMaterial(material);
+        
+        this.attachChild(model);
     }
+
+    public void updateSpeed(double modifier) {
+        this.speed *= modifier; 
+    }
+
+    public void findPath() {
+    }
+    
     
 }
--- src/containing/Main.java
+++ src/containing/Main.java
@@ -25,14 +25,12 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(Vector3f.ZERO, 1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        
+        Material mat2 = new Material(assetManager, 
+                "Common/MatDefs/Misc/Unshaded.j3md");
+        mat2.setColor("Color", ColorRGBA.Red);
+        AGV agv = new AGV("AAA", mat2, assetManager );
+        rootNode.attachChild(agv);
     }
 
     @Override
