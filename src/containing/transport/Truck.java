f5539598f830be28053235a58c1ec7f86a57cbb5
HackSlash <markaas@live.nl>
2014-11-20 10:38:10 +0100

    tempcommit for merge /w master

--- src/containing/transport/Truck.java
+++ src/containing/transport/Truck.java
@@ -1,6 +1,5 @@
 package containing.transport;
 
-<<<<<<< HEAD
 import com.jme3.asset.AssetManager;
 import com.jme3.material.Material;
 import com.jme3.scene.Spatial;
@@ -11,17 +10,9 @@ public class Truck {
     private AssetManager assetmanager;
     private Material material;
     private Spatial model;
-    public Truck() {
-        
-    }
-    
-    private void initPhysics() {
-=======
-public class Truck {
     
     
     public Truck() {
->>>>>>> initial work on train&truck
         
-    }
+    }    
 }
