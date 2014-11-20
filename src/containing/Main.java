409b7a555270d14d4d0ff181f321af10c92c416a
HackSlash <markaas@live.nl>
2014-11-20 10:43:58 +0100

    mainfix remaster

--- src/containing/Main.java
+++ src/containing/Main.java
@@ -5,11 +5,7 @@ import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
-import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
-import com.jme3.scene.shape.Box;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 public class Main extends SimpleApplication {
