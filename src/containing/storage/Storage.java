0ff27c18884172747357e7c45b93f8141d39dfa0
HackSlash <markaas@live.nl>
2015-01-14 23:17:52 +0100

    Final Merge with kenneth.
    Files: Storage.java & StorageCrane.java

--- src/containing/storage/Storage.java
+++ src/containing/storage/Storage.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package containing.storage;
 
 import com.jme3.asset.AssetManager;
@@ -15,48 +11,48 @@ import com.jme3.scene.shape.Box;
 import containing.AGV;
 import containing.Container;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
-/**
- *
- * @author Driving Ghost
- */
 public class Storage extends Node {
 
     private AssetManager assetManager;
-    private List<Container> cargo;
+    public ArrayList<ArrayList<Stack<Container>>> containers;
     private StorageCrane crane;
-    private Vector3f loc;
+    private Vector3f location;
     public ArrayList<AGV> garageA;
     public ArrayList<AGV> garageB;
+    Container container;
 
     public Storage(AssetManager assetManager, StorageCrane cr, Vector3f l, int amount, int value) {
+        //this does not work xD
+        containers = new ArrayList<ArrayList<Stack<Container>>>();
         this.garageA = new ArrayList<AGV>();
         this.garageB = new ArrayList<AGV>();
         this.assetManager = assetManager;
-        this.loc = l;
+        this.location = l;
         this.crane = cr;
 
+        initHold();
         initLine();
         initAGV(amount, value);
-        
+
         this.attachChild(this.crane);
     }
 
     public void initAGV(int amount, int value) {
         for (int i = 0; i < amount; i++) {
-            AGV agv = new AGV("SA"+ Character.toChars(48 + value)+ "V"+i, assetManager);
+            AGV agv = new AGV("SA" + Character.toChars(48 + value) + "V" + i, assetManager);
             garageA.add(agv);
-            agv.setLocalTranslation(loc.x + 8 - (i *5), loc.y,320);
+            agv.setLocalTranslation(location.x + 8 - (i * 5), location.y, 320);
             this.attachChild(agv);
         }
         for (int i = 0; i < amount; i++) {
-            AGV agv = new AGV("SB"+ Character.toChars(48 + value)+ "V"+i, assetManager);
+            AGV agv = new AGV("SB" + Character.toChars(48 + value) + "V" + i, assetManager);
             garageB.add(agv);
-            agv.setLocalTranslation(loc.x + 8 - (i *5), loc.y,-260);
+            agv.setLocalTranslation(location.x + 8 - (i * 5), location.y, -260);
             agv.rotate(0, -FastMath.PI, 0);
             this.attachChild(agv);
-        } 
+        }
     }
 
     public void initLine() {
@@ -65,18 +61,63 @@ public class Storage extends Node {
         Material containerlines_mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         containerlines_mat.setColor("Color", ColorRGBA.Gray);
         containerlines_geom.setMaterial(containerlines_mat);
-        containerlines_geom.setLocalTranslation(this.loc);
+        containerlines_geom.setLocalTranslation(this.location);
         this.attachChild(containerlines_geom);
     }
 
-    public void Storecargo(Container c) {
-        this.cargo.add(c);
+//    public void Storecargo(Container c) {
+//        this.containers.push(c);
+//    }
+//
+//    public Container Unstorecargo(int id) {
+//        Container c = this.containers.get(id);
+//        this.containers.remove(id);
+//        return c;
+//    }
+    public void addContainer(Container c) {
+        for (int x = 0; x < 6; x++) {
+            for (int z = 0; z < 5; z++) {
+                for (int y = 0; y <= 2; y++) {
+                    if (this.containers.get(x).get(z).size() < 2) {
+                        this.containers.get(x).get(z).push(c);
+                        this.attachChild(c);
+
+                        c.setLocalTranslation(this.location.x - 6.5f + (2.8f * x), this.location.y + (y * 3.8f), this.location.z + 250 - (18.8f * z));
+                        return;
+                    }
+                }
+
+            }
+
+        }
+
     }
 
-    public Container Unstorecargo(int id) {
-        Container c = this.cargo.get(id);
-        this.cargo.remove(id);
-        return c;
+    public void freeLocation() {
+        for (int x = 0; x < 6; x++) {
+            for (int z = 0; z < 28; z++) {
+                for (int y = 0; y <= 2; y++) {
+                    if (this.containers.get(x).get(z).isEmpty()) {
+                    }
+                }
+
+            }
+
+        }
+
+    }
+
+    public void initHold() {
+        // this doesnt really work ...
+
+        for (int x = 0; x < 6; x++) {
+            containers.add(new ArrayList<Stack<Container>>());
+            for (int z = 0; z < 34; z++) {
+                containers.get(x).add(new Stack<Container>());
+
+            }
+        }
+
     }
 
     public StorageCrane Getcranes() {
